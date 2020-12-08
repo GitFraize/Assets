c72467adf58abaa46e366beff2657f511b048144
GitFraize <44468710+GitFraize@users.noreply.github.com>
2020-12-09 01:47:46 +0300

    Add restart

---
+++ Scripts/GameOver.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    public Button restartButton;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<King>() != null)
+        {
+            Destroy(other.gameObject);
+            Time.timeScale = 0;
+            restartButton.gameObject.SetActive(true);
+        }
+    }
+
+    public void restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1;
+        restartButton.gameObject.SetActive(false);
+    }
+}
