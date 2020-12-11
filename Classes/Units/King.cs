9e73ebd1d50952f69d50208fad9e3e1822758bc6
GitFraize <44468710+GitFraize@users.noreply.github.com>
2020-12-11 18:15:14 +0300

    Add check-message

--- Classes/Units/King.cs
+++ Classes/Units/King.cs
@@ -2,18 +2,24 @@
 public class King : MonoBehaviour
 {
     public int x = 4;
-    public int y= 0;
+    public int y = 0;
     public int gY = 0;
 
     public Pattern nowPattern { get; set; }
     public GameObject checkPrefab;
     public Canvas canvas;
-    public void moveKing(int _x,int _y, Pattern _pattern)
+    public void moveKing(int _x, int _y, Pattern _pattern)
     {
-        int X = 0, Y = 0;
-        X = _x != x ? (_x - x) / Mathf.Abs(_x - x) : X = 0;
-        Y = _y != y ? (_y - y) / Mathf.Abs(_y - y) : Y = 0;
-        if (nowPattern.patternNum == _pattern.patternNum) {
+        int X = _x != x ? (_x - x) / Mathf.Abs(_x - x) : X = 0;
+        int Y = _y != y ? (_y - y) / Mathf.Abs(_y - y) : Y = 0;
+
+        if (nowPattern.patternNum >= _pattern.patternNum)
+        {
+            if (nowPattern.patternNum > _pattern.patternNum)
+                if (gY % 12 != 0)
+                    Y = -1;
+                else
+                    Y = 0;
             if (nowPattern.patternMoves[y + Y, x + X])
             {
                 y += Y;
@@ -24,26 +30,28 @@ public class King : MonoBehaviour
             else
                 showCheck();
         }
-        else if(nowPattern.patternMoves[y + Y, x + X])
+        else if (nowPattern.patternNum < _pattern.patternNum)
         {
-            Y = 1;
-            if (y + Y > 15)
+            if (nowPattern.patternMoves[y + Y, x + X])
             {
-                y = 0;
-                gameObject.GetComponentInParent<Board>().deleteOldPattern();
-                nowPattern = _pattern;
+                Y = 1;
+                if (y + Y > 11)
+                {
+                    y = 0;
+                    nowPattern = _pattern;
+                }
+                else
+                    y += Y;
+                x += X;
+                gY += Y;
+                gameObject.transform.Translate(new Vector3(X * 1.3f, 0, Y * 1.3f));
             }
             else
-                y += Y;
-            x += X;
-            gY += Y;
-            gameObject.transform.Translate(new Vector3(X * 1.3f, 0, Y * 1.3f));
+                showCheck();
         }
-        else
-            showCheck();
         if (nowPattern.pattern[y, x] != 0)
         {
-            Destroy(nowPattern.pieces[y, x]);
+            nowPattern.pieces[y, x].GetComponent<Piece>().killPiece();
             nowPattern.pattern[y, x] = 0;
             nowPattern.scanPattern();
         }
@@ -53,6 +61,6 @@ public class King : MonoBehaviour
     {
         GameObject check = Instantiate(checkPrefab);
         check.transform.SetParent(canvas.transform);
-        check.transform.localPosition = new Vector3(Input.mousePosition.x-Screen.width/2,Input.mousePosition.y-Screen.height/2,0);
+        check.transform.localPosition = new Vector3(Input.mousePosition.x - Screen.width / 2, Input.mousePosition.y - Screen.height / 2, 0);
     }
 }
