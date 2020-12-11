eec8f6f0aea7b5f7250b4e125b31e273d4ebc9bb
GitFraize <44468710+GitFraize@users.noreply.github.com>
2020-12-11 18:13:46 +0300

    Rework moving script

--- Scripts/Moving.cs
+++ Scripts/Moving.cs
@@ -2,9 +2,5 @@
 
 public class Moving : MonoBehaviour
 {
-    public float speed;
-    private void Update()
-    {
-        gameObject.transform.Translate(new Vector3(0,0,-speed*Time.deltaTime));
-    }
+    
 }
