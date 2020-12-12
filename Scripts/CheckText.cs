660c6da2b323bd70bd77c55d18aa5acf469d66ad
GitFraize <44468710+GitFraize@users.noreply.github.com>
2020-12-13 02:33:29 +0300

    Optimization messages

--- Scripts/CheckText.cs
+++ Scripts/CheckText.cs
@@ -1,24 +1,41 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class CheckText : MonoBehaviour
 {
     public Text text;
     public float speed = 1;
     public float maxScale = 12;
-    bool oneCheck = false;
 
-    private void Update()
+    void Start()
     {
-        if (gameObject.transform.localScale.y < maxScale && !oneCheck)
+        StartCoroutine(animationUp());
+    }
+    private IEnumerator animationUp()
+    {
+        while (gameObject.transform.localScale.y < maxScale)
         {
             gameObject.transform.localScale += new Vector3(Time.deltaTime * speed, Time.deltaTime * speed, 0);
-        } else
-            oneCheck = true;
-        if (oneCheck && gameObject.transform.localScale.y > 1)
+            if (gameObject.transform.localScale.y >= maxScale)
+            {
+                StopCoroutine(animationUp());
+                StartCoroutine(animationDown());
+            }
+            yield return new WaitForEndOfFrame();
+        }
+    }
+    private IEnumerator animationDown()
+    {
+        while (gameObject.transform.localScale.y > 1)
         {
-            gameObject.transform.localScale -= new Vector3(Time.deltaTime * speed*2, Time.deltaTime * speed*2, 0);
-        }else if(oneCheck)
-            Destroy(gameObject);
+            gameObject.transform.localScale -= new Vector3(Time.deltaTime * speed * 2, Time.deltaTime * speed * 2, 0);
+            if (gameObject.transform.localScale.y <= 1)
+            {
+                StopCoroutine(animationDown());
+                Destroy(gameObject);
+            }
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
