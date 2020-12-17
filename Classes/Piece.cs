8582cf0828749eca65948cf71939df6fba8429b8
GitFraize <44468710+GitFraize@users.noreply.github.com>
2020-12-18 00:16:44 +0300

    Add random rotation to Pieces

--- Classes/Piece.cs
+++ Classes/Piece.cs
@@ -7,6 +7,7 @@ public class Piece : MonoBehaviour
     {
         pId = _pId;
         gameObject.transform.localPosition = new Vector3(0, 0.5f, 0);
+        gameObject.transform.Rotate(new Vector3(0, Random.Range(-90, 90), 0));
     }
     public void killPiece()
     {
